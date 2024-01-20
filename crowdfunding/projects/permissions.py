537d76a5b7bc75d0fdca460fd2c65c49db48f132
goyadev <goyazheng@gmail.com>
2024-01-20 11:06:17 +0800

    updated permission for supported

--- crowdfunding/projects/permissions.py
+++ crowdfunding/projects/permissions.py
@@ -9,5 +9,5 @@ class IsSupporterOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.owner == request.user
+        return obj.supporter == request.user
     
