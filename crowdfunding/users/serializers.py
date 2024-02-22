1ec643c93db7ded301b10afd3e153a886dfaa582
goyadev <goyazheng@gmail.com>
2024-02-22 16:04:43 +0800

    trial user serializer fix

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -5,5 +5,5 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = '__all__'
         extra_kwargs = {'password': {'write_only': True}}
-def create(self, validated_data):
-    return CustomUser.objects.create_user(**validated_data)
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
